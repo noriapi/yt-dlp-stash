8a69aac308a4b0a8d455193dc174c42ad0d845eb
noriapi <70106808+noriapi@users.noreply.github.com>
2026-08-12 16:53:43 +0900

    fix

--- yt_dlp_plugins/postprocessor/stash.py
+++ yt_dlp_plugins/postprocessor/stash.py
@@ -72,7 +72,7 @@ class StashPP(PostProcessor):
             tags = self.stash.find_tags(
                 {"name": {"modifier": "EQUALS", "value": tag_name}}
             )
-            if tags and len(tags) == 0:
+            if len(tags) == 0:
                 tags = [self.stash.create_tag({"name": tag_name})]
             self.tag.extend(tags)
 
