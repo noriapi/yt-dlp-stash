b4e188562584f97433db153e69d79dfd27e630d7
noriapi <70106808+noriapi@users.noreply.github.com>
2026-08-12 16:16:29 +0900

    fix

--- yt_dlp_plugins/postprocessor/stash.py
+++ yt_dlp_plugins/postprocessor/stash.py
@@ -21,7 +21,7 @@ class StashPP(PostProcessor):
         sessioncookie: str = "",
         searchpathoverride: str = "",
         scrapemethod: str = "yt_dlp",
-        default_tags: str = "scrape"
+        default_tags: list[str] = ["scrape"],
         **kwargs,
     ):
         # ⚠ Only kwargs can be passed from the CLI, and all argument values will be string
@@ -37,7 +37,7 @@ class StashPP(PostProcessor):
             stash_args["SessionCookie"] = sessioncookie
         self.stash = StashInterface(stash_args)
         self.searchpathoverride = searchpathoverride
-        self.default_tags = default_tags.split(",")
+        self.default_tags = default_tags
 
     def run(self, info):
         if self.scrapemethod == "stash":
